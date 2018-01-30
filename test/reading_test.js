4cbc62b162feb16a0c622ae05626ba3c31802890
Toucher-F <lianduanb@gmail.com>
2018-01-30 17:36:16 -0500

    add skip and limit in test to handle big collection

--- test/reading_test.js
+++ test/reading_test.js
@@ -2,11 +2,16 @@ const assert = require("assert");
 const User = require("../src/user");
 
 describe("Reading users out of the database", () => {
-  let joe;
+  let joe, maria, alex, zach;
 
   beforeEach(done => {
+    alex = new User({ name: "Alex" });
     joe = new User({ name: "Joe" });
-    joe.save().then(() => done());
+    maria = new User({ name: "Maria" });
+    zach = new User({ name: "Zach" });
+    Promise.all([alex.save(), joe.save(), maria.save(), zach.save()]).then(() =>
+      done()
+    );
   });
 
   it("find all users with a name of joe", done => {
@@ -21,5 +26,19 @@ describe("Reading users out of the database", () => {
       assert(user.name === "Joe");
       done();
     });
+  });
+
+  it("can skip and limit the result set", done => {
+    // -Alex- [Joe Maria] Zach
+    User.find({})
+      .sort({ name: 1 }) // 1 means accending -1 means decending
+      .skip(1)
+      .limit(2)
+      .then(users => {
+        assert(users.length === 2);
+        assert(users[0].name === "Joe");
+        assert(users[1].name === "Maria");
+        done();
+      });
   });
 });
